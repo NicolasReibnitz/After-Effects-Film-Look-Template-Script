4f8616d9d8df2e384218638320a4173d9c479acf
NicolasReibnitz <n.reibnitz@gmail.com>
2025-03-29 02:52:20 -0400

    Removed redundant files

--- src/9. Flicker (news reel).ts
+++
@@ -1,9 +0,0 @@
-// Apply flicker FX to the 'Flicker' layer in the active comp
-namespace _FlickerNewsReel {
-	export function applyEffects(layer: AVLayer) {
-		/* === EXPOSURE === */
-
-		const exposure = layer.effect.addProperty('ADBE Exposure2');
-		Utils.setEffectProp(exposure, 'Exposure', 'wiggle(12, 0.3)', true);
-	}
-}
--- src/9. Flicker.ts
+++
@@ -1,9 +0,0 @@
-// Apply flicker FX to the 'Flicker' layer in the active comp
-namespace _Flicker {
-	export function applyEffects(layer: AVLayer) {
-		/* === EXPOSURE === */
-
-		const exposure = layer.effect.addProperty('ADBE Exposure2');
-		Utils.setEffectProp(exposure, 'Exposure', 'wiggle(12, 0.1)', true);
-	}
-}
